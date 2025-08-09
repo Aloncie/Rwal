a5960e44e3655ade1c7359ed9203095321715d21
p1rat <p1ratggwp@mail.ru>
2025-08-09 22:13:22 +0300

    init

---
+++ build/CMakeFiles/main.dir/link.d
@@ -0,0 +1,277 @@
+main: \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/Scrt1.o \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/crti.o \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/crtbeginS.o \
+  CMakeFiles/main.dir/main.cpp.o \
+  /usr/lib/libcurl.so \
+  /usr/lib/libQt5DBus.so.5.15.17 \
+  /usr/lib/libQt5Core.so.5.15.17 \
+  /usr/lib/libcurl.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libstdc++.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libm.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libm.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libm.so \
+  /usr/lib/libm.so.6 \
+  /usr/lib/libmvec.so.1 \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libgcc_s.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libgcc_s.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libgcc_s.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libgcc_s.so.1 \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/libgcc.a \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/libgcc.a \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libc.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libc.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libc.so \
+  /usr/lib/libc.so.6 \
+  /usr/lib/libc_nonshared.a \
+  /usr/lib/ld-linux-x86-64.so.2 \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libgcc_s.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libgcc_s.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libgcc_s.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libgcc_s.so.1 \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/libgcc.a \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/libgcc.a \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/crtendS.o \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/crtn.o \
+  /usr/lib32/libnghttp3.so.9 \
+  /usr/lib/libnghttp3.so.9 \
+  /usr/lib32/libnghttp2.so.14 \
+  /usr/lib/libnghttp2.so.14 \
+  /usr/lib32/libidn2.so.0 \
+  /usr/lib/libidn2.so.0 \
+  /usr/lib32/libssh2.so.1 \
+  /usr/lib/libssh2.so.1 \
+  /usr/lib32/libpsl.so.5 \
+  /usr/lib/libpsl.so.5 \
+  /usr/lib32/libssl.so.3 \
+  /usr/lib/libssl.so.3 \
+  /usr/lib32/libcrypto.so.3 \
+  /usr/lib/libcrypto.so.3 \
+  /usr/lib32/libgssapi_krb5.so.2 \
+  /usr/lib/libgssapi_krb5.so.2 \
+  /usr/lib32/libzstd.so.1 \
+  /usr/lib/libzstd.so.1 \
+  /usr/lib32/libbrotlidec.so.1 \
+  /usr/lib/libbrotlidec.so.1 \
+  /usr/lib32/libz.so.1 \
+  /usr/lib/libz.so.1 \
+  /usr/lib32/libdbus-1.so.3 \
+  /usr/lib/libdbus-1.so.3 \
+  /usr/lib32/libsystemd.so.0 \
+  /usr/lib/libsystemd.so.0 \
+  /usr/lib/libdouble-conversion.so.3 \
+  /usr/lib32/libicui18n.so.76 \
+  /usr/lib/libicui18n.so.76 \
+  /usr/lib32/libicuuc.so.76 \
+  /usr/lib/libicuuc.so.76 \
+  /usr/lib32/libpcre2-16.so.0 \
+  /usr/lib/libpcre2-16.so.0 \
+  /usr/lib32/libglib-2.0.so.0 \
+  /usr/lib/libglib-2.0.so.0 \
+  /usr/lib/ld-linux-x86-64.so.2 \
+  /usr/lib32/libunistring.so.5 \
+  /usr/lib/libunistring.so.5 \
+  /usr/lib32/libkrb5.so.3 \
+  /usr/lib/libkrb5.so.3 \
+  /usr/lib32/libk5crypto.so.3 \
+  /usr/lib/libk5crypto.so.3 \
+  /usr/lib32/libcom_err.so.2 \
+  /usr/lib/libcom_err.so.2 \
+  /usr/lib32/libkrb5support.so.0 \
+  /usr/lib/libkrb5support.so.0 \
+  /usr/lib32/libkeyutils.so.1 \
+  /usr/lib/libkeyutils.so.1 \
+  /usr/lib32/libresolv.so.2 \
+  /usr/lib/libresolv.so.2 \
+  /usr/lib32/libbrotlicommon.so.1 \
+  /usr/lib/libbrotlicommon.so.1 \
+  /usr/lib32/libcap.so.2 \
+  /usr/lib/libcap.so.2 \
+  /usr/lib32/libicudata.so.76 \
+  /usr/lib/libicudata.so.76 \
+  /usr/lib32/libpcre2-8.so.0 \
+  /usr/lib/libpcre2-8.so.0
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/Scrt1.o:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/crti.o:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/crtbeginS.o:
+
+CMakeFiles/main.dir/main.cpp.o:
+
+/usr/lib/libcurl.so:
+
+/usr/lib/libQt5DBus.so.5.15.17:
+
+/usr/lib/libQt5Core.so.5.15.17:
+
+/usr/lib/libcurl.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libstdc++.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libm.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libm.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libm.so:
+
+/usr/lib/libm.so.6:
+
+/usr/lib/libmvec.so.1:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libgcc_s.so.1:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/libgcc.a:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/libgcc.a:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libc.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libc.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libc.so:
+
+/usr/lib/libc.so.6:
+
+/usr/lib/libc_nonshared.a:
+
+/usr/lib/ld-linux-x86-64.so.2:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libgcc_s.so.1:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/libgcc.a:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/libgcc.a:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/crtendS.o:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/crtn.o:
+
+/usr/lib32/libnghttp3.so.9:
+
+/usr/lib/libnghttp3.so.9:
+
+/usr/lib32/libnghttp2.so.14:
+
+/usr/lib/libnghttp2.so.14:
+
+/usr/lib32/libidn2.so.0:
+
+/usr/lib/libidn2.so.0:
+
+/usr/lib32/libssh2.so.1:
+
+/usr/lib/libssh2.so.1:
+
+/usr/lib32/libpsl.so.5:
+
+/usr/lib/libpsl.so.5:
+
+/usr/lib32/libssl.so.3:
+
+/usr/lib/libssl.so.3:
+
+/usr/lib32/libcrypto.so.3:
+
+/usr/lib/libcrypto.so.3:
+
+/usr/lib32/libgssapi_krb5.so.2:
+
+/usr/lib/libgssapi_krb5.so.2:
+
+/usr/lib32/libzstd.so.1:
+
+/usr/lib/libzstd.so.1:
+
+/usr/lib32/libbrotlidec.so.1:
+
+/usr/lib/libbrotlidec.so.1:
+
+/usr/lib32/libz.so.1:
+
+/usr/lib/libz.so.1:
+
+/usr/lib32/libdbus-1.so.3:
+
+/usr/lib/libdbus-1.so.3:
+
+/usr/lib32/libsystemd.so.0:
+
+/usr/lib/libsystemd.so.0:
+
+/usr/lib/libdouble-conversion.so.3:
+
+/usr/lib32/libicui18n.so.76:
+
+/usr/lib/libicui18n.so.76:
+
+/usr/lib32/libicuuc.so.76:
+
+/usr/lib/libicuuc.so.76:
+
+/usr/lib32/libpcre2-16.so.0:
+
+/usr/lib/libpcre2-16.so.0:
+
+/usr/lib32/libglib-2.0.so.0:
+
+/usr/lib/libglib-2.0.so.0:
+
+/usr/lib/ld-linux-x86-64.so.2:
+
+/usr/lib32/libunistring.so.5:
+
+/usr/lib/libunistring.so.5:
+
+/usr/lib32/libkrb5.so.3:
+
+/usr/lib/libkrb5.so.3:
+
+/usr/lib32/libk5crypto.so.3:
+
+/usr/lib/libk5crypto.so.3:
+
+/usr/lib32/libcom_err.so.2:
+
+/usr/lib/libcom_err.so.2:
+
+/usr/lib32/libkrb5support.so.0:
+
+/usr/lib/libkrb5support.so.0:
+
+/usr/lib32/libkeyutils.so.1:
+
+/usr/lib/libkeyutils.so.1:
+
+/usr/lib32/libresolv.so.2:
+
+/usr/lib/libresolv.so.2:
+
+/usr/lib32/libbrotlicommon.so.1:
+
+/usr/lib/libbrotlicommon.so.1:
+
+/usr/lib32/libcap.so.2:
+
+/usr/lib/libcap.so.2:
+
+/usr/lib32/libicudata.so.76:
+
+/usr/lib/libicudata.so.76:
+
+/usr/lib32/libpcre2-8.so.0:
+
+/usr/lib/libpcre2-8.so.0:
