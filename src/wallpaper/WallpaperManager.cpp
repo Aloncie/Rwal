970d64bca14a449a2ddac73b0d39dd5836b0bdaf
p1rat <p1ratggwp@mail.ru>
2025-08-12 21:44:30 +0300

    update

--- src/wallpaper/WallpaperManager.cpp
+++ src/wallpaper/WallpaperManager.cpp
@@ -10,7 +10,7 @@ namespace fs = std::filesystem;
 
 void save_wallpaper(std::string from){
 	try{
-		std::string to = "/home/p1rat/Pictures" + from.substr(21,41);
+		std::string to = "/home/p1rat/Pictures/rwall" + from.substr(21,41);
 		std::cout << to << std::endl;
 		fs::copy_file(from,to,fs::copy_options::overwrite_existing);	
 	} catch (const std::exception& e){
