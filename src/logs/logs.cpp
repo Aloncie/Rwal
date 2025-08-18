9411dcc6f1facd822430fd7710c630a7aa9ca06b
p1rat <p1ratggwp@mail.ru>
2025-08-18 21:17:34 +0300

    update funcs.cpp/h

--- src/logs/logs.cpp
+++ src/logs/logs.cpp
@@ -5,7 +5,7 @@ std::string Logs::get_current_time(){
 	auto in_time_t = std::chrono::system_clock::to_time_t(now);
 
 	std::stringstream ss;
-	ss << std::put_time(std::localtime(&in_time_t), "[%Y-%m-%d %H:%M:%S]");
+	ss << std::put_time(std::localtime(&in_time_t), "[%Y-%m-%d | %H:%M:%S]");
 	return ss.str();
 }
 
