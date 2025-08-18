a89809fad1338a708eca0bfb10ac62192c6c188b
p1rat <p1ratggwp@mail.ru>
2025-08-18 13:02:45 +0300

    add flag --change

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,15 @@
 #include "startup_flows/startup_flows.h"
+#include "wallpaper/WallpaperManager.h"
+#include <string>
 
-int main(int argc,char **argv){
+int main(int argc,char* argv[]){
 	Flows f;
-	f.core(argc,argv);
+	std::string s;
+	if (argv[1])
+		s = argv[1];
+	if (s == "--change" || s == "-c")
+		refresh_wallpaper(argc,argv,"change");
+	else 
+		f.core(argc,argv);
+	return 0;
 }
