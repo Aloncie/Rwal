513a0807a8f703534e6558c92147620300ad0849
p1rat <p1ratggwp@mail.ru>
2025-12-27 00:12:01 +0300

    add NetworkManager

---
+++ src/net/NetworkManager.cpp
@@ -0,0 +1,54 @@
+#include "NetworkManager.h"
+#include "logs/logs.h"
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <cstring>
+
+struct SocketGuard{
+	int fd;
+	SocketGuard(int s) : fd(s){}
+	~SocketGuard(){
+		if (fd != -1)
+			close(fd);
+	}
+};
+
+NetworkManager& NetworkManager::getInstance(){
+	static NetworkManager instance;
+	return instance;
+}
+
+bool NetworkManager::isAvailable() {
+    auto& logger = Logs::getInstance();
+   	logger.write_logs("Try to check internet connection");
+
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock == -1) {
+        logger.write_logs("Socket creation failed");
+        return false;
+    }
+
+    SocketGuard guard(sock); 
+
+    sockaddr_in server{};
+    server.sin_family = AF_INET;
+    server.sin_port = htons(53);
+    
+    if (inet_pton(AF_INET, "8.8.8.8", &server.sin_addr) <= 0) {
+        logger.write_logs("IP conversion failed");
+        return false;
+    }
+
+    timeval tv{.tv_sec = 3, .tv_usec = 0};
+    setsockopt(sock, SOL_SOCKET, SO_SNDTIMEO, &tv, sizeof(tv));
+
+    if (connect(sock, (struct sockaddr*)&server, sizeof(server)) == 0) {
+        logger.write_logs("Internet check: SUCCESS");
+        return true;
+    }
+
+    logger.write_logs("Internet check: FAILED (No connection to 8.8.8.8:53)");
+    return false;
+}
