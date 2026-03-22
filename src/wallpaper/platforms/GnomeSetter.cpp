22eccb0db55471c836f165eafc59902bfe49b2c2
p1rat <p1ratggwp@mail.ru>
2026-03-22 15:37:53 +0300

    add GnomeSetter

---
+++ src/wallpaper/platforms/GnomeSetter.cpp
@@ -0,0 +1,28 @@
+#include "GnomeSetter.hpp"
+#include "logs/logs.hpp"
+#include <gio/gio.h>
+
+void GnomeSetter::setWallpaper(const std::string& path) override{
+	GFile* file = g_file_new_for_path(path);
+	char* uri = g_file_get_uri(file);
+	GSettings* settings = g_settings_new("org.gnome.desktop.background");
+	const char* key = "picture-uri";
+
+	if (g_settings_set_string(settings, key, uri)){
+		Logs::getInstance().writeLogs("Wallpaper set to: " + uri);	
+		g_settings_sync();
+	}
+	else{
+		Logs::getInstance().writeLogs("Failed to set wallpaper");
+	}
+
+	g_free(uri);
+	g_object_unref(file);
+	g_object_unref(settings);
+}
+
+GnomeSetter::GnomeSetter(){
+	#if !GLIB_CHECK_VERSION(2,36,0)
+	g_type_init();
+	#endif
+}
