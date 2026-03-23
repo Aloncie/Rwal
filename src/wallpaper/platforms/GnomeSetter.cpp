c9d5c09ea7a15053c9499ae08a3a357bb71f9506
p1rat <p1ratggwp@mail.ru>
2026-03-23 19:48:29 +0300

    fix GnomeSetter.cpp

--- src/wallpaper/platforms/GnomeSetter.cpp
+++ src/wallpaper/platforms/GnomeSetter.cpp
@@ -1,24 +1,31 @@
 #include "GnomeSetter.hpp"
-#include "logs/logs.hpp"
+#include "../../logs/logs.hpp"
+
+#ifdef signals
+  #undef signals
+#endif
+
 #include <gio/gio.h>
 
-void GnomeSetter::setWallpaper(const fs::path& path){
-	GFile* file = g_file_new_for_path(path);
-	char* uri = g_file_get_uri(file);
-	GSettings* settings = g_settings_new("org.gnome.desktop.background");
-	const char* key = "picture-uri";
+void GnomeSetter::setWallpaper(const fs::path& path) {
+    GFile* file = g_file_new_for_path(path.c_str());
+    
+    if (!file) {
+        Logs::getInstance().writeLogs("GnomeSetter: Failed to create GFile from path");
+        return;
+    }
 
-	if (g_settings_set_string(settings, key, uri)){
-		Logs::getInstance().writeLogs("Wallpaper set to: " + uri);	
-		g_settings_sync();
-	}
-	else{
-		Logs::getInstance().writeLogs("Failed to set wallpaper");
-	}
+    char* uri = g_file_get_uri(file);
+    GSettings* settings = g_settings_new("org.gnome.desktop.background");
 
-	g_free(uri);
-	g_object_unref(file);
-	g_object_unref(settings);
+    g_settings_set_string(settings, "picture-uri", uri);
+    g_settings_set_string(settings, "picture-uri-dark", uri);
+
+    Logs::getInstance().writeLogs("Wallpaper set to: " + std::string(uri));
+
+    g_free(uri);
+    g_object_unref(settings);
+    g_object_unref(file);
 }
 
 GnomeSetter::GnomeSetter(){
