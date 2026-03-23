5e2b5d79baf3cbd44b8fc223827d660b77ca3cb7
p1rat <p1ratggwp@mail.ru>
2026-03-23 17:33:01 +0300

    fix wallpaper/CMakeLists.txt, WallpaperFactory.cpp

--- src/wallpaper/WallpaperFactory.cpp
+++ src/wallpaper/WallpaperFactory.cpp
@@ -1,23 +1,32 @@
 #include "WallpaperFactory.hpp"
 #include "IWallpaperSetter.hpp"
+#include <memory>
 
-#ifdef _WIN32
-    #include "platforms/WindowsSetter.hpp"
-#elif defined(__linux__)
+#ifdef RWAL_USE_KDE
     #include "platforms/KdeSetter.hpp"
+#elif defined(RWAL_USE_GNOME)
     #include "platforms/GnomeSetter.hpp"
+#elif defined(RWAL_USE_FALLBACK)
+    #include "platforms/LinuxFallbackSetter.hpp"
 #endif
 
 std::unique_ptr<IWallpaperSetter> WallpaperFactory::create(){
+/* soon
 
 	#ifdef _WIN32
 		return std::make_unique<WindowsSetter>();	
 	#elif defined(__APPLE__)		
 		return std::make_unique<MacosSetter>();
-	#else
-		std::string de = std::getenv("XDG_CURRENT_DESKTOP");
-		if (de == "KDE") return std::make_unique<KdeSetter>();
-		else if (de == "GNOME") return std::make_unique<GnomeSetter>();
-		// return std::make_unique<LinuxFallbackSetter>();
-	#endif
+*/
+
+	#ifdef RWAL_USE_KDE
+        return std::make_unique<KdeSetter>();
+    #elif defined(RWAL_USE_GNOME)
+        return std::make_unique<GnomeSetter>();
+    #elif defined(RWAL_USE_FALLBACK)
+        return std::make_unique<LinuxFallbackSetter>();
+    #else
+        return nullptr; 
+    #endif
+
 }
