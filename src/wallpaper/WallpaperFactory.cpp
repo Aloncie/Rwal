4a55001f1189ec356cede2ef2b6edd7e5c8caea3
p1rat <p1ratggwp@mail.ru>
2026-03-27 22:12:00 +0300

    update WallpaperManager.cpp

--- src/wallpaper/WallpaperFactory.cpp
+++ src/wallpaper/WallpaperFactory.cpp
@@ -1,24 +1,25 @@
 #include "WallpaperFactory.hpp
+
 #include <memory>
 
 #include "IWallpaperSetter.hpp"
 
 #ifdef RWAL_USE_KDE
-#include "platforms/KdeSetter.hpp"
+	#include "platforms/KdeSetter.hpp"
 #elif defined(RWAL_USE_GNOME)
-#include "platforms/GnomeSetter.hpp"
+	#include "platforms/GnomeSetter.hpp"
 #elif defined(RWAL_USE_HYPRLAND)
-#include "platforms/HyprlandSetter.hpp"
+	#include "platforms/HyprlandSetter.hpp"
 #endif
 
 std::unique_ptr<IWallpaperSetter> WallpaperFactory::create() {
 #ifdef RWAL_USE_KDE
-  return std::make_unique<KdeSetter>();
+    return std::make_unique<KdeSetter>();
 #elif defined(RWAL_USE_GNOME)
-  return std::make_unique<GnomeSetter>();
+    return std::make_unique<GnomeSetter>();
 #elif defined(RWAL_USE_HYPRLAND)
-  return std::make_unique<HyprlandSetter>();
+    return std::make_unique<HyprlandSetter>();
 #else
-  return nullptr;
+    return nullptr;
 #endif
 }
