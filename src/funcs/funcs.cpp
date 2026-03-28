d6ed80662c6e41b798a954d69fbba73153d4d5fb
p1rat <p1ratggwp@mail.ru>
2026-03-28 11:31:30 +0300

    add exception in random()

--- src/funcs/funcs.cpp
+++ src/funcs/funcs.cpp
@@ -1,11 +1,14 @@
 #include "funcs.hpp"
 #include <random>
 #include <unistd.h>
+#include <stdexcept>
 
-int random(int count){
-	static std::random_device rd;
-    static std::mt19937 gen(rd()); 
-    
+int random(int count) {
+    static std::random_device rd;
+    static std::mt19937 gen(rd());
+    if (count <= 0) {
+        throw std::invalid_argument("Count must be a positive integer");
+    }
     std::uniform_int_distribution<> distrib(1, count);
     return distrib(gen);
 }
