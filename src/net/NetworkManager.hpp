4e5abf87cb0243f9a25afcadfcdd8fc3e6e8d8f8
Bogdan Bakuta <Aloncie@proton.me>
2026-03-29 18:29:40 +0300

    Update NetworkManager.hpp

--- src/net/NetworkManager.hpp
+++ src/net/NetworkManager.hpp
@@ -1,6 +1,6 @@
 #pragma once
 #include "CurlWrapper.hpp"
-#include "settings/config.hpp"
+#include "settings/IConfigReader.hpp"
 #include <optional>
 #include <string>
 #include <filesystem>
@@ -10,12 +10,12 @@ namespace fs = std::filesystem;
 
 class NetworkManager{
 public:
-	NetworkManager(MyCurl& curl, Config& config);
+	NetworkManager(MyCurl& curl, IConfigReader& config);
 	std::optional<fs::path> fetchImage(std::string keyword);
 	bool isAvailable();
 	std::string craftUrl(std::string keyword, std::optional<std::string> page = std::nullopt);
 	~NetworkManager() = default;
 private:
 	MyCurl& m_curl;
-	Config& m_config;
+	IConfigReader& m_config;
 };
