06d6fd45650e9349430ac2ea883e800e92d18ccd
p1rat <p1ratggwp@mail.ru>
2026-04-02 21:18:35 +0300

    moving run logic into /Application(new) from main.cpp

---
+++ src/Application/Application.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+class Application {
+public:
+    Application() = default;
+    ~Application() = default;
+    int run(int argc, char *argv[]);
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,60 +1,6 @@
-#include "AppController/AppController.hpp"
-#include "keywords/keywords.hpp"
-#include "logs/logs.hpp"
-#include "navigator/navigator.hpp"
-#include "net/CurlWrapper.hpp"
-#include "net/NetworkManager.hpp"
-#include "settings/config.hpp"
-#include "settings/settings.hpp"
-#include "ui/cli/UIManager.hpp"
-#include "ui/menus/menus.hpp"
-#include "wallpaper/IWallpaperSetter.hpp"
-#include "wallpaper/WallpaperFactory.hpp"
-#include "wallpaper/WallpaperManager.hpp"
+#include "Application/Application.hpp"
 
-#include <QCoreApplication>
-#include <QObject>
-
-int main(int argc, char* argv[]) {
-    QCoreApplication::setApplicationName("Rwal");
-    QCoreApplication::setOrganizationName("Aloncie");
-    QCoreApplication app(argc, argv);
-
-    UIManager um;
-    Config config;
-    Keywords keywords(um, config);
-    CurlWrapper curl;
-    Timer timer;
-    NetworkManager nm(curl, config);
-    WallpaperFactory wf;
-    std::unique_ptr<IWallpaperSetter> env = wf.create();
-    WallpaperManager wm(um, keywords, nm, *env);
-
-    if (argc > 1 && (strcmp(argv[1], "--change") == 0 || strcmp(argv[1], "-c") == 0)) {
-        wm.refresh("change");
-        return 0;
-    }
-
-    um.initUI();
-
-    auto mainMenu = std::make_unique<MainMenu>(um, keywords, wm);
-    auto settingsMenu = std::make_unique<SettingsMenu>(timer, wm);
-    auto keywordsMenu = std::make_unique<KeywordsMenu>(keywords, um, config);
-    auto timerMenu = std::make_unique<TimerMenu>(timer);
-
-    Navigator navigator;
-    navigator.registerMenu("main", std::move(mainMenu));
-    navigator.registerMenu("settings", std::move(settingsMenu));
-    navigator.registerMenu("keywords", std::move(keywordsMenu));
-    navigator.registerMenu("timer", std::move(timerMenu));
-
-    navigator.start("main");
-
-    AppController controller(&navigator, um);
-    Logs::init(um);
-    Logs::getInstance().writeLogs("Rwal started");
-
-    int one = app.exec();
-    um.shutdownUI();
-    return one;
+int main(int argc, char *argv[]) {
+	Application app;
+	return app.run(argc, argv);
 }
