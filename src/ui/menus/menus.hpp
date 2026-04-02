61bf0c290d52c8c1c1328764cc55b42742ea8284
p1rat <p1ratggwp@mail.ru>
2026-04-02 21:26:37 +0300

    update menus.hpp use 'inline static const' instead simple string getValidChoices return link instead of object's copy

--- src/ui/menus/menus.hpp
+++ src/ui/menus/menus.hpp
@@ -20,7 +20,7 @@ public:
     virtual ~Menu() = default;
     virtual std::vector<std::string> getLines() = 0;
     virtual MenuResponce handleInput(const std::string& input) = 0;
-    virtual std::string getValidChoices() const = 0;
+    virtual const std::string getValidChoices() const = 0;
 };
 
 class MainMenu : public Menu {
@@ -28,26 +28,26 @@ private:
     UIManager& m_ui;
     Keywords& m_keywords;
     WallpaperManager& m_wm;
-    const std::string m_validChoices = "1234q";
+    inline static const std::string m_validChoices = "1234q";
 
 public:
     MainMenu(UIManager& ui, Keywords& keywords, WallpaperManager& wm);
     std::vector<std::string> getLines() override;
     MenuResponce handleInput(const std::string& input) override;
-    std::string getValidChoices() const override { return m_validChoices; }
+    const std::string getValidChoices() const override { return m_validChoices; }
 };
 
 class SettingsMenu : public Menu {
 private:
     Timer& m_timer;
     WallpaperManager& m_wm;
-    const std::string m_validChoices = "12q";
+    inline static const std::string m_validChoices = "12q";
 
 public:
     SettingsMenu(Timer& timer, WallpaperManager& wm);
     std::vector<std::string> getLines() override;
     MenuResponce handleInput(const std::string& input) override;
-    std::string getValidChoices() const override { return m_validChoices; }
+    const std::string getValidChoices() const override { return m_validChoices; }
 };
 
 class KeywordsMenu : public Menu {
@@ -55,23 +55,23 @@ private:
     Keywords& m_keywords;
     UIManager& m_ui;
 	IConfigReader& m_config;
-    const std::string m_validChoices = "armq";
+    inline static const std::string m_validChoices = "armq";
 
 public:
     KeywordsMenu(Keywords& keywords, UIManager& ui, IConfigReader& config);
     std::vector<std::string> getLines() override;
     MenuResponce handleInput(const std::string& input) override;
-    std::string getValidChoices() const override { return m_validChoices; }
+    const std::string getValidChoices() const override { return m_validChoices; }
 };
 
 class TimerMenu : public Menu {
 private:
     Timer& m_timer;
-    const std::string m_validChoices = "nhd";
+    inline static const std::string m_validChoices = "nhd";
 
 public:
     TimerMenu(Timer& timer);
     std::vector<std::string> getLines() override;
     MenuResponce handleInput(const std::string& input) override;
-    std::string getValidChoices() const override { return m_validChoices; }
+    const std::string getValidChoices() const override { return m_validChoices; }
 };
