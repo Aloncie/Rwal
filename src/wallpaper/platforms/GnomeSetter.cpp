d6601d2a9fda8fb97e74d36f5ca489b87c689dd3
p1rat <p1ratggwp@mail.ru>
2026-04-03 10:45:14 +0300

    update GnomeSetter.cpp add DE checker add more correct returning and logging

--- src/wallpaper/platforms/GnomeSetter.cpp
+++ src/wallpaper/platforms/GnomeSetter.cpp
@@ -4,6 +4,16 @@
 #include <gio/gio.h>
 
 bool GnomeSetter::setWallpaper(const fs::path& path) {
+    if (std::getenv("XDG_CURRENT_DESKTOP") == nullptr ||
+        std::string(std::getenv("XDG_CURRENT_DESKTOP")).find("GNOME") == std::string::npos) {
+        Logs::getInstance().writeLogs(
+            "Skipping GnomeSetter: XDG_CURRENT_DESKTOP does not indicate GNOME.");
+        return false;
+    }
+    if (!g_file_test(path.c_str(), G_FILE_TEST_EXISTS)) {
+        Logs::getInstance().writeLogs("GnomeSetter: File does not exist - " + path.string());
+        return false;
+    }
     GFile* file = g_file_new_for_path(path.c_str());
 
     if (!file) {
