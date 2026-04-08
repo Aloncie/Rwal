debf5f732bc04c394f48a6421b1ae1eab800c2cd
p1rat <p1ratggwp@mail.ru>
2026-04-08 21:28:12 +0300

    add LinuxFileSystem.hpp

---
+++ src/internal/filesystem/LinuxFileSystem.hpp
@@ -0,0 +1,28 @@
+#include "IFileSystem.hpp"
+
+#include <vector>
+#include <filesystem>
+
+namespace fs = std::filesystem;
+
+class LinuxFileSystem : public IFileSystem{
+private:
+	mutable std::string m_LastError;
+public:
+	
+	LinuxFileSystem() = default;
+	~LinuxFileSystem() override = default;
+
+	bool exists(const fs::path& path) const override;
+	bool createDirectories(const fs::path& path) override;
+	bool removeAll(const fs::path& path) override;
+	bool copyFile(const fs::path& current, const fs::path& dest) const override;
+	std::vector<fs::path> listDirectory(const fs::path& path, const std::string& prefix = "") const override;
+
+	fs::path getAppLocalDataLocation() const override;
+	fs::path getPicturesLocation() const override;
+	std::string getApplicationName() const override;
+	
+	std::string getLastError() const override;
+	void clearError() const override;
+};
