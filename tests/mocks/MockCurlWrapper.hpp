e475d504d1abd3a6726f08dcb850fe15704dd709
p1rat <p1ratggwp@mail.ru>
2026-04-08 20:26:37 +0300

    fix MockCurlWrapper.hpp

--- tests/mocks/MockCurlWrapper.hpp
+++ tests/mocks/MockCurlWrapper.hpp
@@ -11,11 +11,13 @@ namespace fs = std::filesystem;
 class MockCurlWrapper : public CurlWrapper {
 public:
 	using CurlWrapper::CurlWrapper;
+	explicit MockCurlWrapper(Logs& logs) : CurlWrapper(logs) {}
+
 	~MockCurlWrapper() override = default;
 
 	MOCK_METHOD(void, getRequest, (std::string url), (override));
 	MOCK_METHOD(std::string, getData, (std::string paragraph, std::string str), (override));
-	MOCK_METHOD(std::optional<std::filesystem::path>, downloadImage, (std::string image_url), (override));
+	MOCK_METHOD(std::optional<fs::path>, downloadImage, (const std::string& image_url), (override));
 
 };
 
