7cef223eada4abd18e92af9da2b85c40e5758766
p1rat <p1ratggwp@mail.ru>
2026-04-09 16:03:32 +0300

    add flag and update Logs Application: add --clear-logs flag
    Logs update:
    refresh() - now it public&virtual, returns bool, doesn't take path(take it from private variable of class)
    getCurrentTime() - make const, update logic via Qt
    getLogs() - make const
    constructor - update logic for refresh logs

--- tests/mocks/MockLogs.hpp
+++ tests/mocks/MockLogs.hpp
@@ -4,6 +4,9 @@
 #include <gmock/gmock.h>
 #include <string_view>
 #include <string>
+#include <filesystem>
+
+namespace fs = std::filesystem;
 
 class MockLogs : public Logs {
 public:
@@ -11,7 +14,8 @@ public:
 	
 	explicit MockLogs() : Logs() {}
 	MOCK_METHOD(void, writeLogs, (std::string_view message), (override));
-	MOCK_METHOD(std::string, getLogs, (int LinesCount), (override));
+	MOCK_METHOD(std::string, getLogs, (const int& LinesCount), (const, override));
+	MOCK_METHOD(bool, refresh, (fs::path& logs_path), (override));
 
 	bool contains(std::string_view substring) {
 		return lastLogMessage.find(substring) != std::string::npos;
