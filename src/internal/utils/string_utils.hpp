c3e4c8d10d0a37644d99f0182e1008b3ddeb760a
Aloncie <Aloncie@proton.me>
2026-04-10 19:13:49 +0300

    update rwal::utils::string::format and add --set-keywords

--- src/internal/utils/string_utils.hpp
+++ src/internal/utils/string_utils.hpp
@@ -2,29 +2,34 @@
 #include <string>
 #include <algorithm>
 #include <vector>
+#include <iostream>
+#include <cctype>
 
 namespace rwal::utils::string{
 	inline void format(std::string& str){
-		if (str.empty())
+		if (str.empty()){
 			return;
-		const char* whitelist = " \t\n\r\v\f";
-		size_t first = str.find_first_not_of(whitelist);
+		}
+		const char* trim_chars = " \t\n\r\f\v";
+
+		size_t first = str.find_first_not_of(trim_chars);
 		if (first == std::string::npos){
+			// string is only trim_chars
 			str.clear();
 			return;
 		}
-		size_t last = str.find_last_not_of(whitelist);
-		str = str.substr(first,(last-first+1));
-		
-		str.erase(std::remove_if(str.begin(), str.end(), [](unsigned char c) {
-            return !std::isalnum(c) && c != ' ' && c != '-' && c != '+';
-        }), str.end());	
-		
-		first = str.find_first_not_of(whitelist);
-        if (first != std::string::npos) {
-            last = str.find_last_not_of(whitelist);
-            str = str.substr(first, (last - first + 1));
-        }
+
+		// Cut off trailing trim chars	
+		size_t last = str.find_last_not_of(trim_chars);
+		str = str.substr(first, last - first + 1);
+
+		// these symbols can be used in keyword, so we don't need to replace them
+		std::string allowed_special = "-+_";
+		for (char& c : str){
+			if (!std::isalnum(static_cast<unsigned char>(c)) && allowed_special.find(c) == std::string::npos){
+				c = ' ';
+			}
+		}
 	}
 	std::vector<std::string> split_by_space(const std::string& input);
 }
