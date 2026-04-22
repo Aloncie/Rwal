85ec80196d008940fc64b2a42d0efe177c4e3e25
Aloncie <Aloncie@proton.me>
2026-04-22 21:39:03 +0300

    refactor(NetworkManager): use std::format in craftUrl()

--- src/net/NetworkManager.hpp
+++ src/net/NetworkManager.hpp
@@ -16,7 +16,7 @@ public:
 	NetworkManager(CurlWrapper& curl, IConfigReader& config, Logs& logs) : m_curl(curl), m_config(config),m_logs(logs) {}
 	virtual std::optional<fs::path> fetchImage(std::string_view keyword);
 	virtual bool isAvailable();
-	virtual std::string craftUrl(std::string_view keyword, std::optional<std::string>& page = std::nullopt);
+	virtual std::string craftUrl(std::string_view keyword, const std::optional<std::string>& page = std::nullopt);
 	virtual ~NetworkManager() = default;
 private:
 	Logs& m_logs;
