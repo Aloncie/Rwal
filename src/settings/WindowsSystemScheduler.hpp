94c1b02813c324ebaae342358bf52c6c0bb5c5f0
Aloncie <Aloncie@proton.me>
2026-04-26 21:44:52 +0300

    wip: create WindowsSystemScheduler.cpp&.hpp

---
+++ src/settings/WindowsSystemScheduler.hpp
@@ -0,0 +1,24 @@
+#pragma once
+#include "logs/logs.hpp"
+#include "ISystemScheduler.hpp"
+
+#include <optional>
+#include <string>
+#include <taskschd.h>
+
+class WindowsSystemScheduler : public ISystemScheduler {
+private:
+	ITaskService* pService = nullptr;
+	ITaskFolder* pFolder = nullptr;
+public:
+    explicit WindowsSystemScheduler(Logs& logs) : m_logs(logs) {}
+	~WindowsSystemScheduler() override;
+	std::string get() const override;
+	std::string set(const std::string& value) override;
+protected:
+	bool create() override;
+	bool reload() const override;
+	bool start() const override;
+	bool disable() const override;
+	std::optional<bool> status() const override;
+};
