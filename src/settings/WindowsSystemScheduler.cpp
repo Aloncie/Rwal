da7a4f8be9c80da457b6d1d9ecec36fd0599f88a
Aloncie <Aloncie@proton.me>
2026-04-28 21:59:45 +0300

    declare all needly smart pointers in WindowsSystemScheduler.hpp to use in class methods, add object of Logs in private class(fix)

--- src/settings/WindowsSystemScheduler.cpp
+++ src/settings/WindowsSystemScheduler.cpp
@@ -2,79 +2,89 @@
 
 WindowsSystemScheduler::WindowsSystemScheduler(Logs& logs) : m_logs(logs){
 	// Init COM
-	CoInitializeEx(NULL, COINIT_MULTITHREADED);
-	CoCreateInstance(CLSID_CTaskScheduler, NULL, CLSCTX_INPROC_SERVER, IID_ITaskScheduler, (void**)&pService);
+	HRESULT hr = CoInitializeEx(NULL, COINIT_MULTITHREADED);
+	if (hr != S_OK) {
+		m_logs.writeLogs(rwal::logs::types::Error, rwal::logs::modules::WindowsSystemScheduler, "Failed to init COM");
+	}
+	hr = CoCreateInstance(CLSID_CTaskScheduler, NULL, CLSCTX_INPROC_SERVER, IID_ITaskScheduler, (void**)&pServicePtr);
+	if (hr != S_OK) {
+		m_logs.writeLogs(rwal::logs::types::Error, rwal::logs::modules::WindowsSystemScheduler, "Failed to create COM instance");
+	}
 	// Connect to server
 	pService->Connect(VARIANT(), VARIANT(), VARIANT(), VARIANT());
 	// Get root folder
-	pService->GetFolder(_bstr_t(L"\\").c_str(), &pFolder);
+	pService->GetFolder(_bstr_t(L"\\").c_str(), &pFolderPtr);
+}
+
+WindowsSystemScheduler::~WindowsSystemScheduler() {
+	CoUninitialize();
 }
 
 bool WindowsSystemScheduler::create() {
-	ITaskDefinition* pTask = nullptr;
+	ITastDefinitionPtr pTask;
+
 	pService->NewTask(0, &pTask);
     pTask->SetApplicationName(L"Rwal");
     pTask->SetWorkingDirectory(L"");
     pTask->SetPriority(1);
-
-	ITaskActions* pActions = nullptr;
+	
+	ITaskActionsPtr pActions; 
 	pTask->get_Actions(&pActions);
 
-	IExecAction* pExecAction = nullptr;
+	IExecActionPtr pExecAction;
     pActions->Create(L"Exec", &pExecAction);
 	pExecAction->SetPath(L"rwal.exe");
     pExecAction->SetArguments(L"change");
-    pExecAction->Release();
 }
 
 bool WindowsSystemScheduler::start() const {
-	IRegisteredTask* pTask = nullptr;
+	IRegisteredTaskPtr pTask;
 	if (pFolder->GetTask(L"Rwal", &pTask) == S_OK) {
 		TASK_STATE state;
 		if (pTask->get_State(&state) == S_OK && state == TASK_STATE_DISABLED) {
 			return false;
 		}
-		pTask->Release();
     }
     return true;
 }
 
 bool WindowsSystemScheduler::reload() const {
-    IRegisteredTask* pTask = nullptr;
+    IRegisteredTaskPtr pTask;
     if (pFolder->GetTask(L"Rwal", &pTask) == S_OK) {
         pTask->Delete(0);
-        pTask->Release();
     }
     return true;
 }
 
 bool WindowsSystemScheduler::disable() const {
-    IRegisteredTask* pTask = nullptr;
-    if (pFolder->GetTask(L"Rwal", &pTask) == S_OK) {
-        pTask->Disable(0);
-        pTask->Release();
+	if (m_pFolder == nullptr) return false;
+    IRegisteredTaskPtr pTask;
+	HRESULT hr = pFolder->GetTask(L"Rwal", &pTask);
+    if (SUCCEEDED(hr) && pTask != nullptr) {
+		hr = pTask->put_Enabled(VARIANT_FALSE);
+		return SUCCEEDED(hr);
     }
-    return true;
+    return false;
 }
 
 std::string WindowsSystemScheduler::get() const {
 	TaskTiming getTiming("Rwal", TaskScheduleType::None); {
     TaskTiming info;
-    IRegisteredTask* pTask = nullptr;
+    IRegisteredTaskPtr pTask;
     
     if (pRootFolder->GetTask(_bstr_t(name.c_str()), &pTask) != S_OK) return info;
 
-    ITaskDefinition* pDef = nullptr;
+    ITaskDefinitionPtr pDef;
     pTask->get_Definition(&pDef);
 
-    ITriggerCollection* pTriggers = nullptr;
+    ITriggerCollectionptr pTriggers;
     pDef->get_Triggers(&pTriggers);
 
     long count = 0;
     pTriggers->get_Count(&count);
 
     if (count > 0) {
-        ITrigger* pTrigger = nullptr;
+        ITriggerPtr pTrigger;
         pTriggers->get_Item(1, &pTrigger);
 
         BSTR start;
@@ -87,56 +97,45 @@ std::string WindowsSystemScheduler::get() const {
 
         if (type == TASK_TRIGGER_DAILY) info.type = TaskScheduleType::Daily;
         else if (type == TASK_TRIGGER_TIME) info.type = TaskScheduleType::Hourly;
-        
-        pTrigger->Release();
     }
 
-    pTriggers->Release();
-    pDef->Release();
-    pTask->Release();
     return info;
 }
 
 std::string WindowsSystemScheduler::set(const std::string& value) {
-	IRegisteredTask* pTask = nullptr;
+	IRegisteredTaskPtr pTask;
     if (pRootFolder->GetTask(_bstr_t("Rwal", &pTask) != S_OK) return;
 
-    ITaskDefinition* pDef = nullptr;
+    ITaskDefinitionPtr pDef;
     pTask->get_Definition(&pDef);
 
-    ITriggerCollection* pTriggers = nullptr;
+    ITriggerCollectionPtr pTriggers;
     pDef->get_Triggers(&pTriggers);
     pTriggers->Clear();
 
     if (type != TaskScheduleType::None) {
-        ITrigger* pTrigger = nullptr;
+        ITriggerPtr pTrigger;
         
         if (type == TaskScheduleType::Daily) {
             pTriggers->Create(TASK_TRIGGER_DAILY, &pTrigger);
-            IDailyTrigger* pDaily = nullptr;
+            IDailyTriggerPtr pDaily;
             pTrigger->QueryInterface(IID_IDailyTrigger, (void**)&pDaily);
             pDaily->put_DaysInterval(1);
-            pDaily->Release();
         } 
         else if (type == TaskScheduleType::Hourly) {
             pTriggers->Create(TASK_TRIGGER_TIME, &pTrigger);
             IRepetitionPattern* pRep = nullptr;
             pTrigger->get_Repetition(&pRep);
             pRep->put_Interval(_bstr_t(L"PT1H"));
-            pRep->Release();
         }
 
         pTrigger->put_StartBoundary(_bstr_t(startTime.c_str()));
-        pTrigger->Release();
     }
 
-    IRegisteredTask* pUpdatedTask = nullptr;
+    IRegisteredTaskPtr pUpdatedTask;
     pRootFolder->RegisterTaskDefinition(_bstr_t(name.c_str()), pDef, TASK_UPDATE, 
         _variant_t(), _variant_t(), TASK_LOGON_INTERACTIVE_TOKEN, _variant_t(L""), &pUpdatedTask);
 
-    if (pUpdatedTask) pUpdatedTask->Release();
-    pTriggers->Release();
-    pDef->Release();
-    pTask->Release();
+    if (!pUpdatedTask) return "Failed to update task";
 }
 
--- src/settings/WindowsSystemScheduler.hpp
+++ src/settings/WindowsSystemScheduler.hpp
@@ -10,15 +10,33 @@
 #pragma comment(lib, "taskschd.lib")
 #pragma comment(lib, "comsuppw.lib")
 
+// Use smart pointers for COM to avoid memory leaks in class and methods variables.
+//
+// Don't use memory management manually(Release() method) because it can cause memory leak or crash app.
+//
+// For methods - declare them before methods body with class and 'Ptr' postfix. 
+// E.g. use 'ITaskServicePtr' instead of a pointer to ITaskService interface.
+
+// Declare smart pointers for COM interfaces.
 _COM_SMARTPTR_TYPEDEF(ITaskService, __uuidof(ITaskService));
+_COM_SMARTPTR_TYPEDEF(ITaskFolder, __uuidof(ITaskFolder));
+_COM_SMARTPTR_TYPEDEF(IRegisteredTask, __uuidof(IRegisteredTask));
+_COM_SMARTPTR_TYPEDEF(ITriggerCollection, __uuidof(ITriggerCollection));
+_COM_SMARTPTR_TYPEDEF(ITaskDefinition, __uuidof(ITaskDefinition));
+_COM_SMARTPTR_TYPEDEF(ITrigget, __uuidof(ITriger));
+_COM_SMARTPTR_TYPEDEF(IExecAction, __uuidof(IExecAction));
+_COM_SMARTPTR_TYPEDEF(IDailyTrigget, __uuidof(IDailyTrigger));
+_COM_SMARTPTR_TYPEDEF(ITaskActions, __uuidof(ITaskActions));
 
 class WindowsSystemScheduler : public ISystemScheduler {
 private:
-	ITaskService m_pService;
-	ITaskFolder m_pFolder;
+	// Class smart pointers for COM interfaces.
+	ITaskServicePtr m_pService;
+	ITaskFolderPtr m_pFolder;
+	Logs& m_logs;
 public:
     explicit WindowsSystemScheduler(Logs& logs) : m_logs(logs) {}
-	~WindowsSystemScheduler() override {};
+	~WindowsSystemScheduler() override;
 	std::string get() const override;
 	std::string set(const std::string& value) override;
 protected:
