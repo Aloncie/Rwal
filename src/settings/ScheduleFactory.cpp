fb6294a46bbc2da187fbbe8abcfe2291aef5d7f3
Aloncie <Aloncie@proton.me>
2026-05-04 21:41:02 +0300

    add WindowsSystemSchdule to ScheduleFactory

--- src/settings/ScheduleFactory.cpp
+++ src/settings/ScheduleFactory.cpp
@@ -2,11 +2,15 @@
 
 #ifdef __linux__
     #include "LinuxSystemSchedule.hpp"
+#ifdef WIN32
+	#include "WindowsSystemSchedule.hpp"
 #endif
 
 std::unique_ptr<ISystemSchedule> createPlatformSchedule(Logs& logs) {
 #ifdef __linux__
     return std::make_unique<LinuxSystemSchedule>(logs);
+#ifdef WIN32
+	return std::make_unique<WindowsSystemSchedule>(logs);
 #else
     return std::nullptr;
 #endif
